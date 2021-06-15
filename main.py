78903bf419c13da7e2fdea437a9868e15feabb09
Arnaud <mazier-stg@anatoscope.com>
2021-06-15 12:10:22 +0200

    add many features: exception management, right pathfile for export, right format for the xlsx file, keep features of interest, authorship, multi road compatibility (tested on b40 and a4), etc...

--- main.py
+++ main.py
@@ -1,23 +1,36 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# =============================================================================
+# Created By  : Arnaud Mazier
+# Created Date: 15/06/2021
+# Institution: University of Luxembourg
+# Contact: ziemnono@gmail.com
+# Project: Esch 2022
+# Funding: This work was supported by the European Union’s Horizon 2020 research
+# and innovation programme under the Marie Sklodowska-Curie grant agreement No. 764644.
+# =============================================================================
+"""This file has been built for downloading a xml file giving road traffic information
+in Luxembourg. Then relevant features can be extracted  into a xlsx file for post-treatment."""
+
 import os
 import urllib.request
 import xml.etree.ElementTree as et
+from datetime import datetime
 
 import pandas as pd
 
 
-# from lxml import etree as et
-
-
 def read_url(url, xml_filename):
-    # read the data from the URL and create an xml file
-
-    # open a connection to a URL using urllib
+    """read the data from the URL and create an xml file"""
+    # Open a connection to a URL using urllib
     web_url = urllib.request.urlopen(url)
 
-    # get the result code and print it
+    # Get the result code and print information
     print("result code: " + str(web_url.getcode()))
+    dt_string = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
+    print("if 200, " + url + " successfully reached the " + dt_string)
 
-    # read the data from the URL and print it and get rid of xmlns which is troublesome
+    # Read the data from the URL and print it and get rid of xmlns which is troublesome
     xmlns = ' xmlns="http://datex2.eu/schema/2/2_0"'
     data = web_url.read().decode("utf-8").replace(xmlns, '')
     f = open(xml_filename, "w+")
@@ -26,61 +39,99 @@ def read_url(url, xml_filename):
 
 
 def read_xml(xml_filename):
-    # Check if the file exists, parse the file and extract the relevant data
-    # return a dictionary with the correct elements
-
+    """Check if the file exists, parse the file and extract the relevant data
+     return a dictionary with the correct elements"""
+    # Parse the xml file
     if not os.path.exists(xml_filename):
         return
     tree = et.parse(xml_filename)
     root = tree.getroot()
 
-    keys = ["publicationTime", "latitude", "longitude", "vehicleFlowRate"]  # all keys to be extracted from xml
-    latitude_key = ["49.50394", "49.50413", "49.501606", "49.501408"]  # the desired latitude
-    longitude_key = ["5.9460163", "5.9458966", "5.9444437", "5.9445815"]  # the desired longitude
+    features_list = ["publicationTime", "value", "latitude", "longitude", "offsetDistance",
+                     "directionBoundOnLinearSection", "roadNumber", "distanceAlong", "speed", "percentage",
+                     "vehicleFlowRate"]  # all keys to be extracted from xml
     dict = {}
-    count = 0  # count to simply avoid to have 2 identical keys, it will change maybe later on
-    take_next = False  # variable indicating that if we find a longitude or latitude present in our key then we can take
-    # the next key otherwise we pass to the next longitude or latitude
+    for keys in features_list:
+        if keys == "distanceAlong":
+            # distanceAlong is present 2 times, we only take one
+            dict[keys] = [element.text for count, element in enumerate(root.findall(".//" + keys)) if
+                          count % 2 == 0]
+        elif keys == "value":
+            # we only want the english version
+            dict["value"] = [element.text for count, element in enumerate(root.findall(".//" + keys)) if
+                             count % 2 == 0]
+            dict["lang"] = [element.attrib["lang"] for count, element in enumerate(root.findall(".//" + keys)) if
+                            count % 2 == 0]
+        elif keys == "offsetDistance":
+            # offsetDistance is present 2 times, we only take one
+            dict[keys] = [element.text for count, element in enumerate(root.findall(".//" + keys + "/" + keys)) if
+                          count % 2 == 0]
+        else:
+            dict[keys] = [element.text for element in root.findall(".//" + keys)]
+    # publicationTime is present only on time in the file, we multiply it for the excel layout
+    dict["publicationTime"] = dict["publicationTime"] * max([len(dict[i]) for i in dict])
 
-    for elem in root.iter():
-        if elem.tag == keys[0]:
-            dict[keys[0]] = elem.text
-        if elem.tag == keys[1] and elem.text in latitude_key:
-            dict[keys[1] + " " + str(count)] = elem.text
-            take_next = True
-        elif elem.tag == keys[1]:
-            take_next = False
-        if elem.tag == keys[2] and elem.text in longitude_key:
-            dict[keys[2] + " " + str(count)] = elem.text
-            take_next = True
-        elif elem.tag == keys[2]:
-            take_next = False
-        if elem.tag == keys[3] and take_next:
-            dict[keys[3] + " " + str(count)] = elem.text
-            count += 1
     return dict
 
 
 def to_excel(dict, xlsx_filename):
-    # Generate an excel file with the given data
-    df = pd.DataFrame(data=dict, index=[0])
+    """Generate an excel file with the given data"""
+    # I noticed that sometimes the dataset is empty, in this case we just write an empty dictionary
+    try:
+        df = pd.DataFrame(data=dict)
+        len_data = len(dict["publicationTime"])
+    except ValueError:
+        dict = {}
+        len_data = 0
+        df = pd.DataFrame(data=dict)
 
+    # Create a Pandas Excel writer using XlsxWriter as the engine.
+    writer = pd.ExcelWriter(xlsx_filename, engine='xlsxwriter')
+
+    # If the xlsx file already exists, append at the end of it (a bit ugly but read mode is only possible with openpyxl
     if not os.path.exists(xlsx_filename):
-        df.to_excel(xlsx_filename, sheet_name=xlsx_filename, index=False)
+        df.to_excel(writer, sheet_name=xlsx_filename, index=False)
     else:
         old_df = pd.read_excel(xlsx_filename, engine="openpyxl", sheet_name=xlsx_filename)
         updated_df = old_df.append(df)
-        updated_df.to_excel(xlsx_filename, sheet_name=xlsx_filename, index=False)
+        updated_df.to_excel(writer, sheet_name=xlsx_filename, index=False)
+
+    # Get the worksheet object.
+    worksheet = writer.sheets[xlsx_filename]
+
+    # Set the column width and format.
+    for idx, col in enumerate(df):  # loop through all columns
+        series = df[col]
+        max_len = max((
+            series.astype(str).map(len).max(),  # len of largest item
+            len(str(series.name))  # len of column name/header
+        )) + 1  # adding a little extra space
+        worksheet.set_column(idx, idx, max_len)  # set column width
+
+    # Close the Pandas Excel writer and output the Excel file.
+    writer.save()
+    print(xlsx_filename, "successfully saved, " + str(len_data) + " lines have been added")
 
 
 if __name__ == '__main__':
-    url = 'https://www.cita.lu/info_trafic/datex/trafficstatus_b40'
-    xml_filename = "trafficstatus_b40.xml"
-    xlsx_filename = "trafficstatus_b40.xlsx"
+    # To work in the current directory
+    if __file__ == "main.py":
+        os.chdir(os.getcwd())
+    else:
+        os.chdir(os.path.dirname(__file__))
 
-    # to work in the current directory
-    os.chdir(os.getcwd())
+    b40_url = 'https://www.cita.lu/info_trafic/datex/trafficstatus_b40'
+    b40_xml_filename = "trafficstatus_b40.xml"
+    b40_xlsx_filename = "trafficstatus_b40.xlsx"
 
-    read_url(url, xml_filename)
-    data = read_xml(xml_filename)
-    to_excel(data, xlsx_filename)
+    a4_url = 'https://www.cita.lu/info_trafic/datex/trafficstatus_a4'
+    a4_xml_filename = "trafficstatus_a4.xml"
+    a4_xlsx_filename = "trafficstatus_a4.xlsx"
+
+    read_url(b40_url, b40_xml_filename)
+    data = read_xml(b40_xml_filename)
+    to_excel(data, b40_xlsx_filename)
+
+    read_url(a4_url, a4_xml_filename)
+    data = read_xml(a4_xml_filename)
+    to_excel(data, a4_xlsx_filename)
